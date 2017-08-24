2775406a52f3c4775ed88dc44cd3c2bf7154662d
Maxwell Cady <maxwell.josef@gmail.com>
2017-08-23 19:57:29 -0700

    add mongoose functionality for api

--- server/server.js
+++ server/server.js
@@ -7,6 +7,20 @@ var {User} = require('./models/user');
 
 var app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
-})
+});
