232ff5a97c7ab978d2994e06778aa26d70f6461e
Maxwell Cady <maxwell.josef@gmail.com>
2017-08-25 19:50:44 -0700

    Add test cases for get todos by id

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/todos/:id', (req, res) => {
   }
   Todo.findById(id).then((todo) => {
     if (!todo) {
-      res.status(400).send();
+      res.status(404).send();
     }
 
     res.send({todo})
