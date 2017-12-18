58e58628d6b8556c32ba3c164219b64fe77f74f3
Maxwell Cady <maxwell.josef@gmail.com>
2017-12-17 18:01:59 -0800

    add post /users route

--- server/server.js
+++ server/server.js
@@ -90,7 +90,19 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+})
 
+// POST /users
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 })
 
 app.listen(port, () => {
